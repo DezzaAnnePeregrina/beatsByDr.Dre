fe8977e24c3e20c20f3ac84d33ff98afab8bc6ad
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2023-12-15 22:54:10 +0800

    banner and join list frame

--- js/main.js
+++ js/main.js
@@ -12,3 +12,22 @@ window.onscroll = () =>{
 
     prevScroll = currentScrollPos
 }
+
+ // banner
+document.addEventListener(window.onscroll, () =>{
+    document.getElementByClassName('img_beats').classList.add('start-animation')
+})
+
+//joining list
+const inputElement = document.getElementById('email')
+const errorMess =  document.getElementById('error_mess')
+
+inputElement.addEventListener('keyup', (e) =>{
+    if(e.key === 'Enter' && !e.target.value.includes('@')){
+        inputElement.classList.add('email-error')
+        errorMess.textContent = "Invalid email"
+    } else{
+        inputElement.classList.remove('email-error')
+        errorMess.textContent = ''
+    }
+})
