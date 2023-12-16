ef581d119dab7ffb4e4a37e36caff956482ae763
DezzaAnnePeregrina <peregrinadezzaanne@gmail.com>
2023-12-16 15:49:22 +0800

    banner fixed

--- js/main.js
+++ js/main.js
@@ -6,17 +6,62 @@ window.onscroll = () =>{
 
     if(prevScroll > currentScrollPos){
         document.getElementById('fixed_header').classList.remove('closed')
+        document.getElementById('logo').classList.remove('minimize')
     } else{
         document.getElementById('fixed_header').classList.add('closed')
+        document.getElementById('logo').classList.add('minimize')
     }
 
     prevScroll = currentScrollPos
 }
 
  // banner
-document.addEventListener(window.onscroll, () =>{
-    document.getElementByClassName('img_beats').classList.add('start-animation')
-})
+const animatedShaded1 = document.getElementById('shaded1')
+const animatedShaded2 = document.getElementById('shaded2')
+
+const animatedBeats1 = document.getElementById('beats1')
+const animatedBeats2 = document.getElementById('beats2')
+
+const animatedImgBeats = document.getElementById('img_beats')
+
+const observer = new IntersectionObserver( e =>{
+    e.forEach(e =>{
+        // const sA = style.animation
+
+        if(e.isIntersecting){
+
+            animatedShaded1.style.animation = 'shadeAnimation 1.5s forwards'
+            animatedShaded2.style.animation = 'shadeAnimation 1.5s forwards'
+
+            animatedBeats1.style.animation = 'fadeAnimation 1.7s forwards'
+            animatedBeats2.style.animation = 'fadeAnimation 1.7s forwards'
+
+            animatedImgBeats.style.animation ='imgFadeAnimation 3s forwards'
+
+        } else{
+
+            animatedShaded1.style.animation = ''
+            animatedShaded1.style.opacity = 1
+            animatedShaded2.style.animation = ''
+            animatedShaded2.style.opacity = 1
+
+            animatedBeats1.style.animation = ''
+            animatedBeats1.style.opacity = 1
+            animatedBeats2.style.animation = ''
+            animatedBeats2.style.opacity = 1
+
+            animatedImgBeats.style.animation =''
+            animatedImgBeats.style.opacity = 1
+            
+        }
+    })
+}, { threshold: 0.5})
+
+observer.observe(animatedShaded1)
+observer.observe(animatedShaded2)
+observer.observe(animatedBeats1)
+observer.observe(animatedBeats2)
+observer.observe(animatedImgBeats)
 
 //joining list
 const inputElement = document.getElementById('email')
@@ -25,9 +70,11 @@ const errorMess =  document.getElementById('error_mess')
 inputElement.addEventListener('keyup', (e) =>{
     if(e.key === 'Enter' && !e.target.value.includes('@')){
         inputElement.classList.add('email-error')
+        inputElement.style.borderColor = 'red' 
         errorMess.textContent = "Invalid email"
     } else{
         inputElement.classList.remove('email-error')
         errorMess.textContent = ''
+        inputElement.style.borderColor = 'green' 
     }
 })
